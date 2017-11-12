6b93ffc1a5c0e2747c8352dafa5e8e8e28749dc5
DGWerlod <ddeanda333@gmail.com>
2017-11-11 21:49:28 -0800

    add template interpreter

--- builder.js
+++ builder.js
@@ -35,12 +35,41 @@ var loadFile = function(name) {
 
 var update = function() {
     if (adjectives && adverbs && nouns && verbs) {
+        var tempUse = [[1,3],"What ",5,1," ",3,"!"];//Math.floor(Math.random()*tempUse.length);
+    	var response = "";
+    	var randomWords = []
+    	for (i = 0; i < tempUse[0].length; i++) {
+    		switch(tempUse[0][i]) {
+    			case 1: randomWords.push(adjectives[Math.floor(Math.random()*adjectives.length)]); break;
+    			case 2: randomWords.push(adverbs[Math.floor(Math.random()*adverbs.length)]); break;
+    			case 3: randomWords.push(nouns[Math.floor(Math.random()*nouns.length)]); break;
+    			case 4: randomWords.push(verbs[Math.floor(Math.random()*verbs.length)]); break;
+    			case 5: case 6: case 7: break;
+    			default: console.log("Wrong Expression (Assign)");
+    		}
+    	}
+    	var pointer = 0;
+        console.log(randomWords);
+    	for (j = 1; j < tempUse.length; j++) {
+    		switch(tempUse[j]) {
+    			case 1: case 2: case 3: case 4: response += randomWords[pointer]; pointer++; break;
+    			case 5: response += a_an(randomWords[pointer],randomWords[pointer+1]); break;
+    			case 6: response += a_ex(randomWords[pointer]); break;
+    			case 7: response += verb_plr(randomWords[pointer],randomWords[pointer+1]); break;
+    			default: response += tempUse[j];
+    		}
+    	}
+        
+        quoteDestination.innerHTML = response;
+        var randP = Math.floor(Math.random()*people.length);
+        authorDestination.innerHTML = "-" + people[randP];
+        
+        /*
         var randAdj = Math.floor(Math.random()*adjectives.length);
         var randN = Math.floor(Math.random()*nouns.length);
         var ind_art = a_an(adjectives[randAdj], nouns[randN]);
-	var randP = Math.floor(Math.random()*people.length);
-	quoteDestination.innerHTML = "What " + ind_art + adjectives[randAdj] + " " + nouns[randN] + "!";
-	authorDestination.innerHTML = "-" + people[randP];
+        quoteDestination.innerHTML = "What " + ind_art + adjectives[randAdj] + " " + nouns[randN] + "!";
+        */
     }
     
 }
@@ -57,16 +86,14 @@ var a_an = function(string, noun){ 	//returns the indefinite article using the n
 var a_ex = function(noun){ 	//returns an indefinite article depending on the plurality of the noun
     if (noun[noun.length - 1] == 's') {
 	    return '';
-    } 
-    else {
+    } else {
 	return 'a ';
     }
 }
-var verb_plr = function(verb, noun){ //returns the verb with an s after it if the subject is singular
-	if (noun[noun.length-1] != 's'){
+var verb_plr = function(verb, noun) { //returns the verb with an s after it if the subject is singular
+	if (noun[noun.length-1] != 's') {
 		return verb + 's';
-	}
-	else{
+	} else {
 		return verb;
 	}
 }
